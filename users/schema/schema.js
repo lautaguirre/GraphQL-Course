3d75365b9c68d2eb2a9fe78a756d2fea708f7803
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-09-15 21:34:02 -0300

    user project init ; added schema

---
+++ users/schema/schema.js
@@ -0,0 +1,28 @@
+const GraphQL = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
+} = GraphQL;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+
+      }
+    },
+  },
+});

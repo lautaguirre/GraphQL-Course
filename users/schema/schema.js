3b67cb7b4aec5f07dac5787416d797580c5608b9
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-09-19 23:14:41 -0300

    added editUser mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -93,6 +93,20 @@ const mutation = new GraphQLObjectType({
 
         return response.data;
       }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      async resolve(parentValue, args) {
+        const response = await axios.patch(`http://localhost:3000/users/${args.id}`, args);
+
+        return response.data;
+      }
     }
   },
 });

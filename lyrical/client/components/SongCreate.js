b576a9b040fd894a689a0fec32b5e8864ad9943d
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-09-22 21:57:51 -0300

    created addSong and deleteSong mutations ;

--- lyrical/client/components/SongCreate.js
+++ lyrical/client/components/SongCreate.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -9,13 +14,31 @@ class SongCreate extends Component {
     };
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+      refetchQueries: [{ query }],
+    }).then(() => {
+      hashHistory.push('/');
+    }).catch(() => {
+      console.log('GRAPH ERROR');
+    });
+  }
+
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
+
         <h3>Create a new song</h3>
 
-        <form>
-          <label htmlFor="songTitle" >Song Title:</label>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label htmlFor="songTitle">Song Title:</label>
+
           <input
             id="songTitle"
             onChange={event => this.setState({ title: event.target.value })}
@@ -27,4 +50,13 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      id,
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);

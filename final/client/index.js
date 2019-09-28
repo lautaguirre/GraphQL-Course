0b25ca6b9f465bfea36c409fcd09c7b87fdcb204
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-09-28 00:33:34 -0300

    added header ; added logout mutation

--- final/client/index.js
+++ final/client/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Router, hashHistory, IndexRoute } from 'react-router';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import App from './components/App';
 
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: (o) => o.id,
 });
 

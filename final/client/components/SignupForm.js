c23e063d495b19b02eaa2f19ed3787d25f2edbb0
Lautaro Aguirre <lauta.nahuel94@gmail.com>
2019-09-28 13:23:12 -0300

    added signup form and mutation

--- final/client/components/SignupForm.js
+++ final/client/components/SignupForm.js
@@ -3,10 +3,10 @@ import { graphql } from 'react-apollo';
 import { hashHistory } from 'react-router';
 
 import AuthForm from './AuthForm';
-import mutation from '../mutations/login';
+import mutation from '../mutations/signup';
 import query from '../queries/CurrentUser';
 
-class LoginForm extends Component {
+class SignupForm extends Component {
   constructor(props) {
     super(props);
 
@@ -34,7 +34,7 @@ class LoginForm extends Component {
   render() {
     return (
       <div>
-        <h3>Login</h3>
+        <h3>Signup</h3>
 
         <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)} />
       </div>
@@ -42,4 +42,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(mutation)(SignupForm);
